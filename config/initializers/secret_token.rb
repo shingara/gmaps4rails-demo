7a28dd8da38a5baf9e51a2fd4948b5c5ae7ad930
Cyril Mougel <cyril.mougel@gmail.com>
2012-09-11 15:13:20 +0200

    rails project create

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gmaps4railsDemo::Application.config.secret_token = 'ba7ae642bd776f454f0b0f017d85d4e1a20aec286406668d96c140ec37b3a2d2d5ef4f011a050920557be51437b14ac82041cc5c1afff12f5206c1e8ded60d2c'
